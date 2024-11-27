18c0afdec76c6869664a25934021e4910fa9c1ba
maraiii <mariaeduantunes200281@gmail.com>
2024-11-27 19:58:39 -0300

    NavBar (Problem Fixed)

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,23 +1,23 @@
 import logo from "../assets/kevinRushLogo.png";
-import {FaLinkedin} from "react-icons/fa";
-import {FaGithub} from "react-icons/fa";
-import {FaSquareXTwitter} from "react-icons/fa";
-import {FaInstagram} from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
-    <div className="flex flex-shrink-0 items-center"> 
-    <img src={logo} alt="logo"></img>
-    
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo"></img></div>
+
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin/> 
-        <FaGithub/>
-        <FaSquareXTwitter/>
-        <FaInstagram/>
+          <FaLinkedin />
+          <FaGithub />
+          <FaTwitter />
+          <FaInstagram />
         </div>
 
-    </div>
+      
     </nav>
   )
-}
+};
